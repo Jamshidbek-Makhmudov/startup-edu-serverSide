cbebb0b06c1ecb4fe2699cfb32da55a93d1966ad
Jamshidbek Makhmudov <101079499+Jamshidbek-Makhmudov@users.noreply.github.com>
2023-12-27 14:48:14 +0900

    added stripe payment to admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { AdminController } from './admin.controller';
+import { AdminService } from 'src/admin/admin.service';
+import { AdminController } from 'src/admin/admin.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Instructor, InstructorSchema } from '../instructor/schemas/instructor.schema';
+import { Instructor, InstructorSchema } from 'src/instructor/schemas/instructor.schema';
 import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { Course, CourseSchema } from 'src/course/schemas/course.schema';
+import { StripeModule } from 'nestjs-stripe';
 
 
 @Module({
@@ -16,6 +17,7 @@ import { Course, CourseSchema } from 'src/course/schemas/course.schema';
       { name: User.name, schema: UserSchema },
       { name: Course.name, schema: CourseSchema },
     ]),
+    StripeModule.forRoot({ apiKey: process.env.STRIPE_SECRET_KEY, apiVersion: '2022-11-15' }),
   ],
   controllers: [AdminController],
   providers: [AdminService]
