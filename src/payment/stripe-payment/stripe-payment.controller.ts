0755d7b648957169019f384d78d814ab18a01546
Jamshidbek Makhmudov <101079499+Jamshidbek-Makhmudov@users.noreply.github.com>
2023-12-27 14:15:22 +0900

    added stripe-payment module

--- src/payment/stripe-payment/stripe-payment.controller.ts
+++ src/payment/stripe-payment/stripe-payment.controller.ts
@@ -1,34 +1,84 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { StripePaymentService } from './stripe-payment.service';
-import { CreateStripePaymentDto } from './dto/create-stripe-payment.dto';
-import { UpdateStripePaymentDto } from './dto/update-stripe-payment.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { StripePaymentService } from 'src/payment/stripe-payment/stripe-payment.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/auth/common/decorators/auth.decorator';
+import { BooksStripePaymentDto } from 'src/payment/stripe-payment/dto/books-stripe-payment.dto';
+import { User } from 'src/user/decorators/user.decorator';
+import { CourseStripePaymentDto } from 'src/payment/stripe-payment/dto/course-stripe-payment.dto';
 
+
+@ApiTags('Stripe-payment')
 @Controller('stripe-payment')
 export class StripePaymentController {
-  constructor(private readonly stripePaymentService: StripePaymentService) {}
+  constructor(private readonly stripePaymentService: StripePaymentService) { }
 
-  @Post()
-  create(@Body() createStripePaymentDto: CreateStripePaymentDto) {
-    return this.stripePaymentService.create(createStripePaymentDto);
-  }
+  /**payment for books */
+  @ApiOperation({ summary: "payment for books" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Post('books')
+  @Auth('USER')
+  paymentBooks(@Body() dto: BooksStripePaymentDto, @User("_id") _id: string) {
+    return this.stripePaymentService.paymentBooks(dto,_id)
+   }
 
-  @Get()
-  findAll() {
-    return this.stripePaymentService.findAll();
-  }
+  /**payment for courses */
+  @ApiOperation({ summary: "payment for courses" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Post('courses')
+  @Auth('USER')
+  paymentCourses(@Body() dto: CourseStripePaymentDto, @User("_id") _id: string) {
+    return this.stripePaymentService.paymentCourses(dto,_id)
+   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.stripePaymentService.findOne(+id);
-  }
+  /**list products */
+  @ApiOperation({ summary: "list products" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Get('list-products')
+  listProducts() {
+    return this.stripePaymentService.listProducts()
+   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStripePaymentDto: UpdateStripePaymentDto) {
-    return this.stripePaymentService.update(+id, updateStripePaymentDto);
-  }
+  /**create subscription */
+  @ApiOperation({ summary: "create subscription" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Post('create-subscription')
+  @Auth("USER")
+  createSubscription(@User("_id") _id:string, @Body() dto:BooksStripePaymentDto) {
+    return this.stripePaymentService.createSubscription(_id,dto)
+   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.stripePaymentService.remove(+id);
-  }
+  /**instructor-balance */
+  @ApiOperation({ summary: "instructor-balance" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Get('instructor-balance')
+  @Auth("INSTRUCTOR")
+  instructorBalance(@User("instructorAccountId") instructorAccountId:string) {
+    return this.stripePaymentService.instructorBalance(instructorAccountId)
+   }
+
+  /**instructor-connect-login */
+  @ApiOperation({ summary: "instructor-connect-login" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Post('instructor-connect-login')
+  @Auth("INSTRUCTOR")
+  instructorConnectLogin(@User("instructorAccountId") instructorAccountId:string) {
+    return this.stripePaymentService.instructorConnectLogin(instructorAccountId)
+   }
+
+  /**apply-coupon */
+  @ApiOperation({ summary: "apply-coupon" })
+  @ApiResponse({ status: 200, type: Promise<String> })
+  @HttpCode(200)
+  @Get('apply-coupon/:id')
+  @Auth()
+  applyCoupon(@Param('id') id:string) {
+    return this.stripePaymentService.applyCoupon(id)
+   }
+
 }
--- src/payment/stripe-payment/stripe-payment.service.ts
+++ src/payment/stripe-payment/stripe-payment.service.ts
@@ -1,26 +1,109 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStripePaymentDto } from './dto/create-stripe-payment.dto';
-import { UpdateStripePaymentDto } from './dto/update-stripe-payment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { InjectStripe } from 'nestjs-stripe';
+import { Course, CourseDocument } from 'src/course/schemas/course.schema';
+import { Model } from 'mongoose';
+import { CustomerService } from 'src/customer/customer.service';
+import Stripe from 'stripe';
+import { BooksStripePaymentDto } from './dto/books-stripe-payment.dto';
+import { CourseStripePaymentDto } from './dto/course-stripe-payment.dto';
+
 
 @Injectable()
 export class StripePaymentService {
-  create(createStripePaymentDto: CreateStripePaymentDto) {
-    return 'This action adds a new stripePayment';
-  }
+    constructor(
+    @InjectStripe() private readonly stripeClient: Stripe,
+    private readonly customerService: CustomerService,
+    @InjectModel(Course.name) private courseModel: Model<CourseDocument>,
+    ) { }
+  
+  /**payment books */
+  async paymentBooks(body: BooksStripePaymentDto, userId: string) {
+    const customer = await this.customerService.getCustomer(userId)
+    const card = await this.customerService.atachPaymentMethod(body.paymentMethod, userId)
+    
+    const paymentIntent = await this.stripeClient.paymentIntents.create({
+      //misol uchun price 20 dollar bolsa stripe buni chunmaydi. oxirida 2ta qoshimcha 0 bilan birga kelsa tushunadi shuning uchun 100ga kopaytirib qoyamiz
+      amount: body.price * 100,
+      currency: 'usd',
+      payment_method: card.id,
+      customer:customer.id
+    })
 
-  findAll() {
-    return `This action returns all stripePayment`;
-  }
+    return paymentIntent.client_secret
+   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} stripePayment`;
-  }
+  /**payment courses */
+  async paymentCourses(body: CourseStripePaymentDto, userId: string) {
+    const customer = await this.customerService.getCustomer(userId)
+    const card = await this.customerService.atachPaymentMethod(body.paymentMethod, userId)
+    const course = (await this.courseModel.findById(body.courseId)).populated("author")
+    
+    const feePrice = (30 / 100) * body.price
+    
+    const paymentIntent = await this.stripeClient.paymentIntents.create({
+      amount: body.price * 100,
+      currency: 'usd',
+      payment_method: card.id,
+      customer: customer.id,
+      application_fee_amount: feePrice * 100,
+      transfer_data: {
+        destination:course.author.instructorAccountId
+      }
+    })
 
-  update(id: number, updateStripePaymentDto: UpdateStripePaymentDto) {
-    return `This action updates a #${id} stripePayment`;
+    return paymentIntent.client_secret
   }
+  
+  /** list products*/
+  async listProducts() {
+    const products = await this.stripeClient.products.list({
+      limit: 3,
+      expand: ['data.default_price'],
+    })
 
-  remove(id: number) {
-    return `This action removes a #${id} stripePayment`;
+    return products.data
   }
+  
+  /**create subscription */
+  async createSubscription(userId: string, body: BooksStripePaymentDto) {
+    const customer = await this.customerService.getCustomer(userId)
+    const card = await this.customerService.atachPaymentMethod(body.paymentMethod, userId)
+    
+    const subscription = await this.stripeClient.subscriptions.create({
+      customer:customer.id,
+      items: [{ price: String(body.price) }],
+      payment_behavior: "default_incomplete",
+      expand: ["latest_invoice.payment_intent"],
+      default_payment_method: card.id,
+      trial_period_days:14,
+    })
+
+    return subscription;
+  }
+  
+  /**instructor balance */
+  async instructorBalance(instructorAccountId: string) {
+    const balance = await this.stripeClient.balance.retrieve({
+      stripeAccount:instructorAccountId
+    })
+
+    return balance
+  }
+  
+  /**instrucor connection login */
+  async instructorConnectLogin(instructorAccountId: string) {
+    const loginLink = await this.stripeClient.accounts.createLoginLink(instructorAccountId)
+    
+    return loginLink.url
+  }
+  
+  /**apply coupon */
+  async applyCoupon(id: string) {
+    const coupon = await this.stripeClient.coupons.retrieve(id)
+    
+    return coupon
+   }
+
+
 }
